5f8afe7b85ed1e1da1ca811170947ad01c7a7c83
Aparajita <aparajita@aparajita.com>
2026-05-04 19:09:34 -0700

    chore: only compile test source with —test

--- scripts/compile.sh
+++ scripts/compile.sh
@@ -2,9 +2,13 @@
 source "$(dirname "$0")/set-java-home.sh"
 [[ "$1" == "clean" ]] && mvn clean -q
 
-goals="generate-sources dependency:copy@copy-native-libs resources:resources kotlin:compile compiler:compile"
-[[ "$1" == "--test" ]] && goals+=" test-compile"
+if [[ "$1" == "--test" ]]; then
+  goals="test-compile"
+else
+  goals="generate-sources dependency:copy@copy-native-libs resources:resources kotlin:compile compiler:compile"
+fi
 
+# shellcheck disable=SC2086
 if mvn $goals -q; then
   echo "SUCCESS"
 else
