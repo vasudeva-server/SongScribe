03dc8155373082eae90e189b2cac119a8fe8b62c
Aparajita <aparajita@aparajita.com>
2026-05-14 19:02:15 -0700

    fix: remove redundant type

--- src/main/java/songscribe/ui/dialog/AboutDialog.java
+++ src/main/java/songscribe/ui/dialog/AboutDialog.java
@@ -39,7 +39,7 @@ public class AboutDialog extends BaseDialog {
     public AboutDialog() {
         super(Strings.get(Strings.DIALOG_ABOUT_TITLE), true, DialogCategory.INFORMATIONAL);
 
-        var gap = (int) FlatLafProps.<Integer>get(FlatLafKeys.DIALOG_COMPONENT_VERTICAL_EXTRA_GAP);
+        int gap = FlatLafProps.get(FlatLafKeys.DIALOG_COMPONENT_VERTICAL_EXTRA_GAP);
 
         var divider = new JPanel();
         divider.setBackground(Color.LIGHT_GRAY);
--- src/main/java/songscribe/ui/dialog/BeatChangeDialog.java
+++ src/main/java/songscribe/ui/dialog/BeatChangeDialog.java
@@ -44,7 +44,7 @@ public class BeatChangeDialog extends AttachmentDialog<BeatChange> {
 
         var row = new JPanel();
         row.setLayout(new BoxLayout(row, BoxLayout.X_AXIS));
-        int extraGap = FlatLafProps.<Integer>get(FlatLafKeys.DIALOG_COMPONENT_HORIZONTAL_EXTRA_GAP);
+        int extraGap = FlatLafProps.get(FlatLafKeys.DIALOG_COMPONENT_HORIZONTAL_EXTRA_GAP);
         row.add(durationCombo);
         row.add(Box.createHorizontalStrut(extraGap));
         row.add(new JLabel("="));
